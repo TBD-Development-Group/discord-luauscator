84b482cfb3342bd059da2c0c5691cbcee115eab9
TBD-Development-Group <boumaazahamza30@gmail.com>
2025-05-23 18:28:47 +0200

    Update random code.js

--- src/random code.js
+++ src/random code.js
@@ -1,15 +1,18 @@
-// Generate random code snippets or junk code for obfuscation
+const junkCodeSnippets = [
+    'local _=function() end; _()',
+    'do local a=1; a=a+1 end',
+    'if math.random()>0 then end',
+    'for i=1,0 do end',
+    'while false do end',
+    'local t={}; table.insert(t,1); table.remove(t,1)',
+    'local f=loadstring(""); if f then f() end'
+];
 
-function randomCode() {
-  const junk = [
-    'local _ = {}',
-    'local __ = {}',
-    'local ___ = math.random(100)',
-    'local skibidijfjfjfj = "obfuscation"',
-    'repeat until false',
-    // add more random junk code snippets here
-  ];
-  return junk[Math.floor(Math.random() * junk.length)];
-}
-
-module.exports = randomCode;
+module.exports = function generateJunkCode() {
+    const count = Math.floor(Math.random() * 3) + 1;
+    let junk = [];
+    for (let i = 0; i < count; i++) {
+        junk.push(junkCodeSnippets[Math.floor(Math.random() * junkCodeSnippets.length)]);
+    }
+    return junk.join('\n');
+};
